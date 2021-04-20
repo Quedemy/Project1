3261e4fb4c34602ce840a8746c0153ce0ab17697
Quedemy <Quedemy@gmail.com>
2021-04-20 07:40:27 +0100

    First Update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Define UI variables
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn  = document.querySelector('.clear-task');
-const filter  = document.querySelector('.filter');
+const clearBtn  = document.querySelector('.clear-tasks');
+const filter  = document.querySelector('#filter');
 const taskInput  = document.querySelector('#task');
 
 
@@ -11,10 +11,69 @@ loadEventListeners();
 
 // load all event listeners
 function loadEventListeners(){
+    //DOM load event
+    document.addEventListener('DOMContentLoaded', getTasks)
+
+
     //Add task event
     form.addEventListener('submit', addTask);
 
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+
+    // Filter task events
+
+    filter.addEventListener('keyup', filterTasks);
+
+
+
 }
+
+//Get Tasks from local storage
+
+function getTasks(){
+    let tasks;
+    if(localStorage.getItem('tasks')=== null){
+        tasks = [];
+
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    } 
+    tasks.forEach(function(task){
+        // create li element
+const li = document.createElement('li');
+
+//Add class
+li.className = 'collection-item';
+
+//create a text node and append to li
+
+li.appendChild(document.createTextNode(task));
+
+//create new link
+
+const link = document.createElement('a');
+
+//Add class
+link.className = 'delete-item secondary-content';
+
+//Add icon html
+link.innerHTML = '<i class ="fa fa-remove"><</i>'
+
+// Append the link to li
+li.appendChild(link);
+
+//Append li to ul
+taskList.appendChild(li);
+
+    });
+
+
+}
+
 
 // Add Task
 function addTask(e){
@@ -47,9 +106,100 @@ li.appendChild(link);
 //Append li to ul
 taskList.appendChild(li);
 
+// 
+storeTaskInLocalStorage(taskInput.value);
+
+
 e.preventDefault();
 
 //Clear input
 taskInput.value = '';
 
+}
+
+//store task
+
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks')=== null){
+        tasks = [];
+
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove Task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();  
+
+            // Remove Task from Local storage
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+        }
+    }
+
+}
+ //Remove task from ls
+ function removeTaskFromLocalStorage(taskItem){
+         
+    let tasks;
+    if(localStorage.getItem('tasks')=== null){
+        //tasks = [];
+        
+
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+       
+    }
+    console.log(tasks);
+    tasks.forEach(function(task, index){
+        console.log(taskItem.innerHTML);
+        console.log(task);
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1)
+            
+        }
+    });
+    console.log(tasks);
+
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        console.log(tasks);
+
+     }
+
+
+
+//Clear Tasks
+
+function clearTasks(){
+    //taskList.innerHTML = '';
+
+    //method 2
+
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+    clearTaskFromLocalStorage()
+}
+function clearTaskFromLocalStorage(){
+    localStorage.clear()
+}
+
+// filter tasks
+
+function filterTasks(e){
+const text = e.target.value.toLowerCase();
+
+document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+    }else{
+        task.style.display = 'none';
+    }
+});
 }
